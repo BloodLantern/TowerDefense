f53c831eefd9eada9670cb47804830c8748ede7d
m.grimardhamel <m.grimardhamel@student.isartdigital.com>
2023-01-04 17:28:09 +0100

    Fixed tower bar square position

--- TowerDefense/src/TowerBarUI.cpp
+++ TowerDefense/src/TowerBarUI.cpp
@@ -2,11 +2,12 @@
 #include "Globals.h"
 
 #define TOWER_BAR_TOWER_SIZE 64
-#define TOWER_BAR_SIZE TOWER_BAR_TOWER_SIZE + 10
+
+#define TOWER_BAR_Y_OFFSET 2
+#define TOWER_BAR_HEIGHT TOWER_BAR_TOWER_SIZE + TOWER_BAR_Y_OFFSET
 #define TOWER_BAR_X Globals::gWindowX
-#define TOWER_BAR_Y Globals::gWindowY + Globals::gWindowHeight - TOWER_BAR_SIZE
+#define TOWER_BAR_Y Globals::gWindowY + Globals::gWindowHeight - TOWER_BAR_TOWER_SIZE - TOWER_BAR_Y_OFFSET
 #define TOWER_BAR_WIDTH Globals::gWindowWidth
-#define TOWER_BAR_HEIGHT TOWER_BAR_SIZE
 
 void TowerBarUI::draw()
 {
@@ -15,9 +16,8 @@ void TowerBarUI::draw()
 	int32_t width = TOWER_BAR_WIDTH;
 	int32_t height = TOWER_BAR_HEIGHT;
 
-	Globals::gDrawList->AddRect(ImVec2(x, y), ImVec2(x + width, y + height), TOWER_BAR_UI_OUTLINE_COLOR);
-
 	Globals::gDrawList->AddRectFilled(ImVec2(x, y), ImVec2(x + width, y + height), TOWER_BAR_UI_BACKGROUND_COLOR);
+	Globals::gDrawList->AddRect(ImVec2(x, y), ImVec2(x + width, y + height), TOWER_BAR_UI_OUTLINE_COLOR);
 
 	handleMouse();
 }
@@ -37,14 +37,9 @@ void TowerBarUI::handleMouse()
 	ImVec2 pos = Globals::gIO->MousePos;
 	if (pos.x > x && pos.y > y && pos.x < x + width && pos.y < y + height)
 	{
-		ImVec2 topLeft(pos.x - x, y);
-
-		topLeft.x = (((int) topLeft.x) / TOWER_BAR_TOWER_SIZE + 1) * TOWER_BAR_TOWER_SIZE;
-
-		ImVec2 bottomRight = topLeft;
-		bottomRight.x += TOWER_BAR_TOWER_SIZE;
-		bottomRight.y += TOWER_BAR_TOWER_SIZE;
-
+		ImVec2 topLeft(pos.x - x, y + TOWER_BAR_Y_OFFSET / 2);
+		topLeft.x = (((int) topLeft.x) / TOWER_BAR_TOWER_SIZE) * TOWER_BAR_TOWER_SIZE + x;
+		ImVec2 bottomRight(topLeft.x + TOWER_BAR_TOWER_SIZE, topLeft.y + TOWER_BAR_TOWER_SIZE);
 		Globals::gDrawList->AddRectFilled(topLeft, bottomRight, TOWER_BAR_UI_TOWER_HOVER_COLOR, 10.f);
 	}
 
@@ -53,7 +48,7 @@ void TowerBarUI::handleMouse()
 
 	ImVec2 clickedPos = Globals::gIO->MouseClickedPos[ImGuiMouseButton_Left];
 
-	if (clickedPos.y > Globals::gWindowY + Globals::gWindowHeight - TOWER_BAR_SIZE && ImGui::IsMouseDragging(ImGuiMouseButton_Left))
+	if (clickedPos.y > Globals::gWindowY + Globals::gWindowHeight - TOWER_BAR_HEIGHT && ImGui::IsMouseDragging(ImGuiMouseButton_Left))
 	{
 
 	}
