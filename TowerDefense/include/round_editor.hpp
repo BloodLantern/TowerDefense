69d64ffb1b41f8007c2e2b91ed37e138a6cb6765
Yohann DEMAISON--REGNE <y.demaisonregne@student.isartdigital.com>
2023-01-13 15:59:03 +0100

    Round editor squeleton, #24

--- TowerDefense/include/round_editor.hpp
+++ TowerDefense/include/round_editor.hpp
@@ -1,7 +1,18 @@
 #pragma once
 
+#include "round.hpp"
+
+#include <vector>
+
 class RoundEditor
 {
+private:
+	static std::vector<RoundInfo> mRoundInfo;
+
+
+	static void DisplayTable();
+	static void DisplayParamColumn(RoundInfo* round);
+
 public:
 	static void Update();
 };
