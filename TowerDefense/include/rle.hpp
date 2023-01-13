6d27213da3e3b4902d2fd328f3478bc8856c7a9e
Yohann DEMAISON--REGNE <y.demaisonregne@student.isartdigital.com>
2023-01-13 09:16:13 +0100

    Make RLE a class instead of namespace

--- TowerDefense/include/rle.hpp
+++ TowerDefense/include/rle.hpp
@@ -2,15 +2,16 @@
 
 #include "playfield.hpp"
 
-namespace RLE
+class RLE
 {
+public:
 	/// <summary>
 	/// Compresses a level to a file
 	/// </summary>
 	/// <param name="pf">Playfield pointer</param>
 	/// <param name="dst">Destination file name</param>
 	/// <returns>True if success, false otherwise (error logged in console)</returns>
-	bool CompressLevel(PlayField* pf, const char* dst);
+	static bool CompressLevel(PlayField* pf, const char* dst);
 
 	/// <summary>
 	/// Decompresses a level from a file
@@ -18,7 +19,7 @@ namespace RLE
 	/// <param name="pf">Playfield pointer</param>
 	/// <param name="dst">Source file name</param>
 	/// <returns>True if success, false otherwise (error logged in console)</returns>
-	bool DecompressLevel(PlayField* pf, const char* src);
+	static bool DecompressLevel(PlayField* pf, const char* src);
 
 	/// <summary>
 	/// Compresses data treated as bytes with RLE into a specified file
@@ -27,7 +28,7 @@ namespace RLE
 	/// <param name="length">Length of data to compress</param>
 	/// <param name="dst">Destination file name</param>
 	/// <returns>True if success, false otherwise (error logged in console)</returns>
-	bool Compress(void* src, size_t length, const char* dst);
+	static bool Compress(void* src, size_t length, const char* dst);
 
 	/// <summary>
 	/// Decompresses a RLE compressed file into a buffer
@@ -35,5 +36,5 @@ namespace RLE
 	/// <param name="dst">Destination pointer</param>
 	/// <param name="src">Source file name</param>
 	/// <returns>True if success, 0 otherwise (error logged in console)</returns>
-	bool Decompress(void* dst, const char* src);
+	static bool Decompress(void* dst, const char* src);
 };
